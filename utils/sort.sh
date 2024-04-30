47f0c95fd41ee13abf7bb056ca67b753d9c794ca
Okinea Dev <81070564+okineadev@users.noreply.github.com>
2024-04-30 05:27:29 +0000

    The sorting script has been improved

--- utils/sort.sh
+++ utils/sort.sh
@@ -11,15 +11,25 @@ else
 
     # Check if the specified file exists
     if [[ ! -f "$file" ]]; then
-        echo "File '$file' not found"
+        echo -e "File '\e[1m$file\e[0m' not found"
         exit 1
     fi
 fi
 
 # Remove duplicates and overwrite the file with a sorted list of words
 for filename in $file; do
+    # Find and print duplicated lines
+    duplicates=$(sort "$filename" | uniq -d)
+    if [[ -n "$duplicates" ]]; then
+        echo -e "\nDuplicated lines in '\e[1m$filename\e[0m':"
+        for word in $duplicates; do
+            echo "- $word"
+        done
+        echo ""
+    fi
+
     sort -u "$filename" -o "$filename"
-    echo "Sorted and removed duplicates in '$filename'"
+    echo -e "Sorted '\e[1m$filename\e[0m'"
 done
 
-echo "Done!"
+echo -e "\e[1;32mDone!\e[0m"
